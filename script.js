f820d9eece99093ea4619d681fec892f200dadf1
darshan-22 <darshannn2003@gmail.com>
2024-12-10 22:29:32 +0530

    edited js code

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const apiKey = "bb5e1a21cde19aa180410e9d2724c869";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+  "https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=bb5e1a21cde19aa180410e9d2724c869&units=metric";
 
 async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid = ${apiKey}`);
-  var data = await response.json;
+  const response = await fetch(apiUrl);
+  var data = await response.json();
+
+  console.log(data);
 }
+
+checkWeather();
